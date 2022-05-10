a6bcbbef336eea46a92b8ead611e8c5b8dddae74
arsen-777 <arsengrigoryan3103@gmail.com>
2022-05-10 14:51:48 +0400

    feat: adds routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import SignUp from "./pages/sign_up/SignUp";
+import Profile from "./pages/profile/Profile";
+import Recipes from "./pages/recipes/Recipes";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/sign-up" element={<SignUp />} />
+				<Route path="/profile" element={<Profile />} />
+				<Route path="/recipes" element={<Recipes />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
